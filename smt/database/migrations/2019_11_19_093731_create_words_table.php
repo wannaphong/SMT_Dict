5f35c3fa93050d2df9e987206f706e46f9f45880
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-11-19 16:46:38 +0700

    Add migration

---
+++ smt/database/migrations/2019_11_19_093731_create_words_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->increments('id'); // ไอดี
+            $table->string('ws'); // คำค้น
+            $table->integer('lang_ws'); // ภาษาต้นทาง
+            $table->string('meaning'); // คำแปล
+            $table->integer('lang_m'); // ภาษาของคำแปล
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
