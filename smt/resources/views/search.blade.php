f1a321b98fa36b4bb4c8fd9b7045b589883bd701
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-11-21 15:06:50 +0700

    update path
    Co-Authored-By: marksolution <marksolution@users.noreply.github.com>

--- smt/resources/views/search.blade.php
+++ smt/resources/views/search.blade.php
@@ -12,7 +12,7 @@
       crossorigin="anonymous"
     />
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
   <script
