e05da94bcd78f63f3dd406a6f3391efa13b22881
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-11-24 21:35:05 +0700

    Update search.blade.php
    Co-Authored-By: marksolution <marksolution@users.noreply.github.com>

--- smt/resources/views/search.blade.php
+++ smt/resources/views/search.blade.php
@@ -44,13 +44,10 @@
     <a class="nav-link active" href="#">HOME</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Login</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">MADE BY SMT</a>
   </li>
 </ul>
     
