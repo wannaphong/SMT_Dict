f3f537f184b2a0a0fe3f456997fd11f0ae471f11
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-11-26 11:16:29 +0700

    Update login.blade.php

--- smt/resources/views/auth/login.blade.php
+++ smt/resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('เข้าสู่ระบบ') }}</div>
+                <div class="card-header">{{ __('เข้าสู่ระบบผู้ดูแลระบบ') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
